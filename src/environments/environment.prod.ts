ace5863f7a0a5ce9d7d9256f46b3b09fc10d78fb
Yevheniia Rakhmatova <yevheniia.rakhmatova@gmail.com>
2021-07-25 14:34:22 +0300

    update buildspec-build.yml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
 
   apiUrls: {
-    quote: '',
+    quote: '', // Put here your QUOTE API BASE URL. E.g.: api or https://quote.com/api
+    share: '', // Put here your SHARE API BASE URL. E.g.: api or https://share.com/api
   },
 };
