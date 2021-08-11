3e15f04e5449af05721c49600d520aa61a97830f
Yevheniia Rakhmatova <yevheniia.rakhmatova@gmail.com>
2021-08-11 21:02:45 +0300

    update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-
+  isAuthorizationEnabled: true,
   apiUrls: {
     quote: '', // Put here your QUOTE API BASE URL. E.g.: api or https://quote.com/api
     share: '', // Put here your SHARE API BASE URL. E.g.: api or https://share.com/api
